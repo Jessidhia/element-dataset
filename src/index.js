07b32113a660d05d9db18fed74a1c1f566b05dc1
Mark Milstein <mark@epiloque.com>
2016-11-30 05:06:48 +0000

    feat(updates): move build/test to @epiloque/weaver

--- src/index.js
+++ src/index.js
@@ -1,66 +1,61 @@
-/* eslint-disable prefer-reflect, camelcase */
+/* eslint-env browser */
 
-'use strict'
+export default function elementDatasetPolyfill () {
+  if (!document.documentElement.dataset &&
+    (
+      !Object.getOwnPropertyDescriptor(Element.prototype, 'dataset') ||
+      !Object.getOwnPropertyDescriptor(Element.prototype, 'dataset').get
+    )
+  ) {
+    const descriptor = {}
 
-function elementDatasetPolyfill () {
-	if (!document.documentElement.dataset &&
-		(
-			!Object.getOwnPropertyDescriptor(Element.prototype, 'dataset')  ||
-			!Object.getOwnPropertyDescriptor(Element.prototype, 'dataset').get
-		)
-	) {
-		const descriptor = {}
+    descriptor.enumerable = true
 
-		descriptor.enumerable = true
+    descriptor.get = function get () {
+      const element = this
+      const map = {}
+      const attributes = this.attributes
 
-		descriptor.get = function () {
-			const element = this
-			const map = {}
-			const attributes = this.attributes
+      function toUpperCase (n0) {
+        return n0.charAt(1).toUpperCase()
+      }
 
-			function toUpperCase (n0) {
-				return n0.charAt(1).toUpperCase()
-			}
+      function getter () {
+        return this.value
+      }
 
-			function getter () {
-				return this.value
-			}
+      function setter (name, value) {
+        if (typeof value !== 'undefined') {
+          this.setAttribute(name, value)
+        } else {
+          this.removeAttribute(name)
+        }
+      }
 
-			function setter (name, value) {
-				if (typeof value !== 'undefined') {
-					this.setAttribute(name, value)
-				}
-				else {
-					this.removeAttribute(name)
-				}
-			}
+      for (let i = 0; i < attributes.length; i += 1) {
+        const attribute = attributes[i]
 
-			for (let i = 0; i < attributes.length; i++) {
-				const attribute = attributes[i]
+        // This test really should allow any XML Name without
+        // colons (and non-uppercase for XHTML)
 
-				// This test really should allow any XML Name without
-				// colons (and non-uppercase for XHTML)
+        if (attribute && attribute.name && (/^data-\w[\w-]*$/).test(attribute.name)) {
+          const name = attribute.name
+          const value = attribute.value
 
-				if (attribute && attribute.name && (/^data-\w[\w\-]*$/).test(attribute.name)) {
-					const name = attribute.name
-					const value = attribute.value
+          // Change to CamelCase
 
-					// Change to CamelCase
+          const propName = name.substr(5).replace(/-./g, toUpperCase)
 
-					const propName = name.substr(5).replace(/-./g, toUpperCase)
+          Object.defineProperty(map, propName, {
+            enumerable: this.enumerable,
+            get: getter.bind({ value: value || '' }),
+            set: setter.bind(element, name)
+          })
+        }
+      }
+      return map
+    }
 
-					Object.defineProperty(map, propName, {
-						enumerable: this.enumerable,
-						get: getter.bind({value: value || ''}),
-						set: setter.bind(element, name)
-					})
-				}
-			}
-			return map
-		}
-
-		Object.defineProperty(Element.prototype, 'dataset', descriptor)
-	}
+    Object.defineProperty(Element.prototype, 'dataset', descriptor)
+  }
 }
-
-export default elementDatasetPolyfill
